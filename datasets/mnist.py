82df91175a00ba206982170e1895fa2f391359b2
iiharu <8288781+iiharu@users.noreply.github.com>
2018-09-07 19:09:32 +0900

    Update

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -4,10 +4,12 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
+from tensorflow.keras import backend as K
+
 CLASSES = 10
 ROWS, COLS, CHS = 28, 28, 1
 
-TRAIN_SIZE = 50000
+TRAIN_SIZE = 60000
 TEST_SIZE = 10000
 
 
@@ -18,11 +20,18 @@ def load_data(normalize=True, flatten=False, one_hot_label=True):
         X_train = X_train.astype(float)
         X_test = X_test.astype(float)
         X_train /= 255
-        Y_test /= 255
+        X_test /= 255
 
     if flatten:
         X_train = X_train.reshape((TRAIN_SIZE, ROWS * COLS * CHS))
         X_test = X_test.reshape((TEST_SIZE, ROWS * COLS * CHS))
+    else:
+        if K.image_data_format() == 'channels_last':
+            X_train = X_train.reshape((TRAIN_SIZE, ROWS, COLS, CHS))
+            X_test = X_test.reshape((TEST_SIZE, ROWS, COLS, CHS))
+        else:
+            X_train = X_train.reshape((TRAIN_SIZE, CHS, ROWS, COLS))
+            X_test = X_test.reshape((TEST_SIZE, CHS, ROWS, COLS))
 
     if one_hot_label:
         Y_train = to_categorical(Y_train, CLASSES)
--- layers/core.py
+++ layers/core.py
@@ -3,15 +3,14 @@
 from tensorflow import keras
 
 
-"""
-Layer function wrappers for Keras Layer.
-
-Do not import all functions in this modules,
-because functions having many parameter is not useful with multiple use.
-"""
-
 def add():
     return keras.layers.Add()
+
+
+def average_pooling2d(pool_size=2, strides=None):
+    return keras.layers.AveragePooling1D(pool_size=pool_size,
+                                         strides=strides,
+                                         padding='same')
 
 
 def average_pooling2d(pool_size=(2, 2), strides=None):
@@ -28,6 +27,20 @@ def concat(axis=-1):
     return keras.layers.Concatenate(axis=axis)
 
 
+def conv1d(filters, kernel_size, strides=1,
+           activation=None, use_bias=True,
+           kernel_initializer='glorot_uniform', bias_initializer='zeros',
+           kernel_regularizer=None, bias_regularizer=None,
+           activity_regularizer=None,
+           kernel_constraint=None, bias_constraint=None):
+    return keras.layers.Conv1D(filters, kernel_size, strides=1, padding='same',
+                               activation=activation, use_bias=use_bias,
+                               kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                               kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+                               activity_regularizer=activity_regularizer,
+                               kernel_constraint=kernel_constraint, bias_constraint=bias_constraint)
+
+
 def conv2d(filters, kernel_size, strides=1,
            activation=None, use_bias=True,
            kernel_initializer='glorot_uniform', bias_initializer='zeros',
@@ -38,6 +51,14 @@ def conv2d(filters, kernel_size, strides=1,
                                kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
                                kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
                                kernel_constraint=kernel_constraint, bias_constraint=bias_constraint)
+
+
+def cropping1d(cropping=(1, 1)):
+    return keras.layers.Cropping1D(cropping)
+
+
+def cropping2d(cropping=((0, 0), (0, 0))):
+    return keras.layers.Cropping2D(cropping)
 
 
 def dense(units, activation=None, use_bias=True,
@@ -60,8 +81,18 @@ def flatten():
     return keras.layers.Flatten()
 
 
+def global_average_pooling1d():
+    return keras.layers.GlobalAveragePooling1D()
+
+
 def global_average_pooling2d():
     return keras.layers.GlobalAveragePooling2D()
+
+
+def max_pooling1d(pool_size=2, strides=None):
+    return keras.layers.MaxPooling1D(pool_size=pool_size,
+                                     strides=strides,
+                                     padding='same')
 
 
 def max_pooling2d(pool_size=(2, 2), strides=None):
@@ -78,3 +109,33 @@ def relu(max_value=None):
 def softmax(axis=-1):
     # return keras.layers.Softmax(axis=axis)
     return keras.layers.Activation(keras.activations.softmax)
+
+
+def up_sampling1d(size=2):
+    return keras.layers.UpSampling1D(size)
+
+
+def up_sampling2d(size=(2, 2)):
+    return keras.layers.UpSampling2D(size)
+
+
+def transposed_conv2d(filters, kernel_size, strides=1,
+                      activation=None, use_bias=True,
+                      kernel_initializer='glorot_uniform', bias_initializer='zeros',
+                      kernel_regularizer=None, bias_regularizer=None,
+                      activity_regularizer=None,
+                      kernel_constraint=None, bias_constraint=None):
+    return keras.layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding='same',
+                                        activation=activation, use_bias=use_bias,
+                                        kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+                                        kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+                                        activity_regularizer=activity_regularizer,
+                                        kernel_constraint=kernel_constraint, bias_constraint=bias_constraint)
+
+
+def zero_padding1d(padding=1):
+    return keras.layers.ZeroPadding1D(padding=padding)
+
+
+def zero_padding2d(padding=(1, 1)):
+    return keras.layers.ZeroPadding2D(padding=padding)
