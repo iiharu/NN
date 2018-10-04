8dd753a59b13ca6e4c6f1d827cd85620f9472472
iiharu <8288781+iiharu@users.noreply.github.com>
2018-10-04 23:01:20 +0900

    Update

--- AlexNet.py
+++ AlexNet.py
@@ -7,3 +7,9 @@ from .layers import
 def build(input_shape=(224, 224, 3), classes=1000):
     inputs = keras.Input(shape=input_shape)
     outputs = keras.layers.Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), padding='same')(inputs)
+    # Local Response Normalization
+    outputs = keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(outputs)
+    outputs = keras.layers.Conv2D(filters=256, kernel_size=(5, 5), padding='same')(outputs)
+    # Local Response Normalization
+
+    
