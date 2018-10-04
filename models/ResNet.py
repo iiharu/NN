14f4de34740096746e84b58af018b1d2c84380c5
iiharu <8288781+iiharu@users.noreply.github.com>
2018-10-04 14:14:25 +0900

    Update

--- models/ResNet.py
+++ models/ResNet.py
@@ -5,31 +5,27 @@ from tensorflow.keras import activations
 from tensorflow.keras import initializers
 from tensorflow.keras import regularizers
 
+from tensorflow.keras import backend as K
+
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Add
+from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import GlobalAveragePooling2D
-
-
-"""
-TODO:
-__init__ のパラメータ調整
-ショートカットコネクションのfilter数, kernel_size, stridesを入力と比較して, なんとかする
-
-layers = 2 (first conv, last dense) + blocks * 2 (not bottleneck)
-layers = 2 (first conv, last dense) + blocks * 3 (not bottleneck)
-blocks がNoneであれば, 均等に割り振る?
-blocks = 2 conv or 3 conv (bottleneck)
-
-cifarはともかく, imagenetは各ブロック数が均等でないので厳しい
-
-"""
+from tensorflow.keras.layers import MaxPooling2D
 
 
 def add(**kwargs):
     return Add(**kwargs)
+
+
+def average_pooling2d(pool_size=(2, 2), strides=None, **kwargs):
+    return AveragePooling2D(pool_size=pool_size,
+                            strides=strides,
+                            padding='same',
+                            **kwargs)
 
 
 def batch_normalization(axis=-1, **kwargs):
@@ -56,6 +52,13 @@ def dense(units, **kwargs):
 
 def global_average_pooling2d(**kwargs):
     return GlobalAveragePooling2D(**kwargs)
+
+
+def max_pooling2d(pool_size=(2, 2), strides=None, **kwargs):
+    return MaxPooling2D(pool_size=(2, 2),
+                        strides=strides,
+                        padding='same',
+                        **kwargs)
 
 
 def relu(**kwargs):
@@ -89,50 +92,97 @@ class ResNet:
     """
     
     def __init__(self, blocks, filters, kernel_size=(3, 3), bottleneck=False):
-        self.blocks = blocks
+        if type(blocks) == list:
+            self.blocks = blocks
+        else:
+            self.blocks = [blocks for i in range(3)]
         if type(filters) == list:
             self.filters = filters
         else:
-            self.filters = [pow(2, i) * filters for i in range(len(blocks))]
+            self.filters = [pow(2, i) * filters for i in range(len(self.blocks))]
         self.kernel_size = kernel_size
         self.bottleneck = bottleneck
+        self.bn_axis = -1 if K.image_data_format() == 'channels_last' else 1
 
-    def residual(self, inputs, filters, kernel_size=(3, 3), down_sampling=False):
-        strides = 2 if down_sampling else 1
-        
+    def residual(self, inputs, filters, sub_sampling=False):
+        strides = 2 if sub_sampling else 1
+        kernel_size = (1, 1) if self.bottleneck else (3, 3)
+        shortcut_filters = 4 * filters if self.bottleneck else filters
+
         outputs = batch_normalization()(inputs)
         outputs = relu()(outputs)
         outputs = conv2d(filters=filters,
                          kernel_size=kernel_size,
-                         strides=strides)(outputs)
-        if down_sampling:
-            inputs = conv2d(filters=filters,
+                         strides=strides
+                         )(outputs)
+        if sub_sampling or self.bottleneck:
+            inputs = conv2d(filters=4 * filters if self.bottleneck else filters,
                             kernel_size=(1, 1),
-                            strides=strides)(inputs)
+                            strides=strides
+                            )(inputs)
         outputs = batch_normalization()(outputs)
         outputs = relu()(outputs)
-        outputs = conv2d(filters=filters, kernel_size=kernel_size)(outputs)
+        outputs = conv2d(filters=filters,
+                         kernel_size=(3, 3),
+                         strides=1
+                         )(outputs)
+        if self.bottleneck:
+            outputs = batch_normalization()(outputs)
+            outputs = relu()(outputs)
+            outputs = conv2d(filters=4*filters,
+                             kernel_size=kernel_size,
+                             strides=1
+                             )(outputs)
         outputs = add()([inputs, outputs])
 
         return outputs
 
+
+    # def residual(self, inputs, filters, kernel_size=(3, 3), down_sampling=False):
+    #     strides = 2 if down_sampling else 1
+    #
+    #     outputs = batch_normalization()(inputs)
+    #     outputs = relu()(outputs)
+    #     outputs = conv2d(filters=filters,
+    #                      kernel_size=kernel_size,
+    #                      strides=strides)(outputs)
+    #     if down_sampling:
+    #         inputs = conv2d(filters=filters,
+    #                         kernel_size=(1, 1),
+    #                         strides=strides)(inputs)
+    #     outputs = batch_normalization()(outputs)
+    #     outputs = relu()(outputs)
+    #     outputs = conv2d(filters=filters, kernel_size=kernel_size)(outputs)
+    #     outputs = add()([inputs, outputs])
+    #
+    #     return outputs
+
     def build(self, input_shape, classes=10):
+        self.imagenet = False
         inputs = keras.Input(shape=input_shape)
 
-        outputs = conv2d(self.filters[0],
-                         kernel_size=self.kernel_size)(inputs)
+        if self.imagenet:
+            outputs = conv2d(filters=self.filters[0],
+                             kernel_size=(7, 7),
+                             strides=2
+                             )(inputs)
+            outputs = max_pooling2d(pool_size=(3, 3), strides=2)(outputs)
+        else:
+            outputs = conv2d(self.filters[0],
+                             kernel_size=self.kernel_size
+                             )(inputs)
 
         for b in range(len(self.blocks)):
             for l in range(self.blocks[b]):
-                if b != 0 and l == 0:
-                    outputs = self.residual(outputs,
-                                            self.filters[b],
-                                            down_sampling=True)
-                else:
-                    outputs = self.residual(outputs,
-                                            self.filters[b])
+                sub_sampling = True if b != 0 and l == 0 else False
+                outputs = self.residual(outputs,
+                                        self.filters[b],
+                                        sub_sampling=sub_sampling)
 
-        outputs = global_average_pooling2d()(outputs)
+        if self.imagenet:
+            outputs = average_pooling2d(pool_size=(2, 2))(outputs)
+        else:
+            outputs = global_average_pooling2d()(outputs)
         outputs = dense(classes)(outputs)
         outputs = softmax()(outputs)
 
@@ -143,24 +193,35 @@ class ResNet:
         return model
 
 
-def ResNet20():
-    return ResNet([3, 3, 3], [16, 32, 64])
+def ResNetB(blocks, filters, kernel_size=(3, 3)):
+    return ResNet(blocks=blocks, filters=filters, kernel_size=kernel_size, bottleneck=True)
 
+def ResNet20():
+    return ResNet(blocks=3, filters=16)
 
 def ResNet32():
-    return ResNet([5, 5, 5], [16, 32, 64])
-
+    return ResNet(blocks=5, filters=16)
 
 def ResNet44():
-    return ResNet([7, 7, 7], [16, 32, 64])
-
+    return ResNet(blocks=7, filters=16)
 
 def ResNet56():
-    return ResNet([9, 9, 9], [16, 32, 64])
+    return ResNet(blocks=9, filters=16)
 
 def ResNet110():
-    return ResNet([18, 18, 18], [16, 32, 64])
+    return ResNet(blocks=18, filters=16)
 
-if __name__ == '__main__':
-    resNet = ResNet110()
-    model = resNet.build(input_shape=(32, 32, 3), classes=10)
+def ResNet29():
+    return ResNet(blocks=3, filters=16, bottleneck=True)
+
+def ResNet47():
+    return ResNet(blocks=5, filters=16, bottleneck=True)
+
+def ResNet65():
+    return ResNet(blocks=7, filters=16, bottleneck=True)
+
+def ResNet83():
+    return ResNet(blocks=9, filters=16, bottleneck=True)
+
+def ResNet164():
+    return ResNet(blocks=18, filters=16, bottleneck=True)
--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,9 @@
 
 from .DenseNet import DenseNet
 from .DenseNet import DenseNetBC
+from .ResNet import ResNet
+from .ResNet import ResNetB
+from .ResNet import ResNet20
+from .ResNet import ResNet32
+from .ResNet import ResNet56
+from .ResNet import ResNet110
