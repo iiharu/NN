9c81ea3134d857706078cd28d6c0aa8739bf17db
iiharu <8288781+iiharu@users.noreply.github.com>
2018-11-19 18:59:17 +0900

    Update layers/activation.py
    - Add elu, selu

--- layers/activation.py
+++ layers/activation.py
@@ -8,6 +8,10 @@ def activation(activation, **kwargs):
     return Activation(activation=activation, **kwargs)
 
 
+def elu(**kwargs):
+    return Activation(activation=activations.elu, **kwargs)
+
+
 def linear(**kwargs):
     return Activation(activation=activations.linear, **kwargs)
 
@@ -15,6 +19,10 @@ def linear(**kwargs):
 def relu(max_value=None, **kwargs):
     # return keras.layers.ReLU(max_value=max_value, **kwargs)
     return Activation(activation=activations.relu, **kwargs)
+
+
+def selu(**kwargs):
+    return Activation(activation=activations.selu, **kwargs)
 
 
 def sigmoid(**kwargs):
